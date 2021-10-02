c2672c082286ed21ad59fcf0e2969510418a2926
Nasir Uddin Khan <uddin6058@gmail.com>
2021-10-02 15:24:31 +0600

    filter/localStorage/Get Data in page

--- JS/script.js
+++ JS/script.js
@@ -11,6 +11,8 @@ let clearBtn = document.querySelector('#clear_task-btn');
 form.addEventListener('submit', addTask);
 taskList.addEventListener('click', removeTask);
 clearBtn.addEventListener('click', clearTask);
+filter.addEventListener('keyup', filterTask);
+document.addEventListener('DOMContentLoaded', getTask);
 
 // define function
 function addTask(e) {
@@ -27,7 +29,8 @@ function addTask(e) {
 		crossLink.innerHTML = 'x';
 		li.appendChild(crossLink);
 		// empty the NEw TASK box
-		// storeTasksIntoLocalStorage(taskInput.value);
+		// Local store
+		storeTasksIntoLocalStorage(taskInput.value);
 		taskInput.value = '';
 	}
 	e.preventDefault();
@@ -54,4 +57,48 @@ function clearTask(e) {
 		// faster way
 		taskList.removeChild(taskList.firstChild);
 	}
+}
+
+// create filterTask function
+function filterTask(e) {
+	let text = e.target.value.toLowerCase();
+	document.querySelectorAll('li').forEach((task) => {
+		let item = task.firstChild.textContent;
+		if (item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
+}
+
+// store in local store
+function storeTasksIntoLocalStorage(task) {
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.push(task);
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// get data in document from local storage
+function getTask() {
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.forEach((task) => {
+		let li = document.createElement('li');
+		li.appendChild(document.createTextNode(task + ''));
+		taskList.appendChild(li);
+		// add close sign
+		let crossLink = document.createElement('a');
+		crossLink.setAttribute('href', '#');
+		crossLink.innerHTML = 'x';
+		li.appendChild(crossLink);
+	});
 }
